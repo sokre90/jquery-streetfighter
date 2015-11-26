2c5b3328a98b091e64bd87542ea578ac32bc5e7f
Kresimir I. Nurnberger <kin9006@aol.com>
2015-11-26 10:55:57 +0100

    street fighter final

--- js/app.js
+++ js/app.js
@@ -32,19 +32,6 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
-	
-	$('.ryu').keydown(function(e) {
-		if(e.keyCode == 88) {
-			$('.ryu-still').hide();
-			$('.ryu-cool').show();
-		}
-	})
-	$('.ryu').keyup(function(e) {
-		if(e.keyCode == 88) {
-			$('.ryu-still').show();
-			$('.ryu-cool').hide();
-		}
-	});
 
 });
 
@@ -53,3 +40,25 @@ $(document).ready(function() {
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
 	}
+	function playCool () {
+		$('#cool-sound')[0].volume = 0.5
+		// $('#cool-sound')[0].load();
+		$('#cool-sound')[0].play();
+	}
+$(document).keydown(function(e) {
+		if(e.keyCode == 88) {
+			playCool();
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+$(document).keyup(function(e) {
+		if(e.keyCode == 88) {
+			$('#cool-sound')[0].pause();
+			$('#cool-sound')[0].load();
+			$('.ryu-still').show();
+			$('.ryu-cool').hide();
+			$('ryu-ready').hide();
+		}
+	});
